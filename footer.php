e7e0cce38219033b17348aec3f37ad71e2b15aa4
Lukas <lukas.vinclav@example.com>
2016-06-19 12:12:23 +0200

    hide footer if no widgets active

--- footer.php
+++ footer.php
@@ -1,22 +1,24 @@
 		<?php do_action( 'listing_manager_front_after_main' ); ?>
 
-		<div class="footer-wrapper">
-			<div class="footer">
-				<div class="footer-inner">
-					<?php if ( is_active_sidebar( 'footer-left') ) : ?>
-						<div class="footer-left">
-							<?php dynamic_sidebar( 'footer-left' ); ?>
-						</div><!-- /.footer-left -->
-					<?php endif; ?>
+		<?php if ( is_active_sidebar( 'footer-left') || is_active_sidebar( 'footer-right' ) ) : ?>
+			<div class="footer-wrapper">
+				<div class="footer">
+					<div class="footer-inner">
+						<?php if ( is_active_sidebar( 'footer-left') ) : ?>
+							<div class="footer-left">
+								<?php dynamic_sidebar( 'footer-left' ); ?>
+							</div><!-- /.footer-left -->
+						<?php endif; ?>
 
-					<?php if ( is_active_sidebar( 'footer-right') ) : ?>
-						<div class="footer-right">
-							<?php dynamic_sidebar( 'footer-right' ); ?>
-						</div><!-- /.footer-right -->
-					<?php endif; ?>
-				</div><!-- /.footer-inner -->
-			</div><!-- /.footer -->
-		</div><!-- /.footer-wrapper -->
+						<?php if ( is_active_sidebar( 'footer-right') ) : ?>
+							<div class="footer-right">
+								<?php dynamic_sidebar( 'footer-right' ); ?>
+							</div><!-- /.footer-right -->
+						<?php endif; ?>
+					</div><!-- /.footer-inner -->
+				</div><!-- /.footer -->
+			</div><!-- /.footer-wrapper -->
+		<?php endif; ?>
 	</div><!-- /.page-wrapper -->
 </body>
 
