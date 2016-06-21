962d2fecfd681d9df381619688f30b35ac7a8c75
Lukas <lukas.vinclav@gmail.com>
2016-06-21 09:30:01 +0200

    style.css fix

--- functions.php
+++ functions.php
@@ -33,7 +33,8 @@ function listing_manager_front_enqueue() {
 	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css' );
 	wp_enqueue_style( 'listing-manager-front-fonts', listing_manager_front_fonts(), array(), '1.0.0' );
 	wp_enqueue_style( 'listing-manager-front', get_template_directory_uri() . '/assets/css/listing-manager-front.css' );
-
+	wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/style.css' );
+	
 	wp_enqueue_script( 'listing-manager-front', get_template_directory_uri() . '/assets/js/listing-manager-front.js', array( 'jquery' ) );
 	wp_enqueue_script( 'masonry', get_template_directory_uri() . '/assets/js/masonry.min.js', array( 'jquery' ) );
 	wp_enqueue_script( 'scrollTo', get_template_directory_uri() . '/assets/js/jquery.scrollTo.min.js', array( 'jquery' ) );
