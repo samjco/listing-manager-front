3832add82c2ee8de0082919171a8c28a2c653255
Lukas <lukas.vinclav@gmail.com>
2016-06-21 11:07:16 +0200

    html5 validator fix

--- header.php
+++ header.php
@@ -89,13 +89,14 @@
 
 				<div class="header-sticky">
 					<div class="site-branding">
-						<div class="site-title">
-							<a href="<?php echo site_url(); ?>">
-								<?php the_custom_logo(); ?>
-								<?php if ( 'blank' !== get_theme_mod( 'header_textcolor', true ) ) : ?>
+						<div class="site-title">						
+							<?php the_custom_logo(); ?>
+							
+							<?php if ( 'blank' !== get_theme_mod( 'header_textcolor', true ) ) : ?>
+								<a href="<?php echo site_url(); ?>">
 									<?php bloginfo( 'name' ); ?>
-								<?php endif; ?>
-							</a>
+								</a>
+							<?php endif; ?>							
 						</div><!-- /.site-title -->
 
 						<?php if ( 'blank' !== get_theme_mod( 'header_textcolor', true ) ) : ?>
